084a4635c87f05d87bc307ed153949c7330e37e4
Antonio Oliva Carceles <116766832+olixva@users.noreply.github.com>
2023-10-09 12:08:31 +0200

    Prueba de conexion terminada e instalacion de los drivers

--- Ejemplo1JDBC/src/Prueba.java
+++ Ejemplo1JDBC/src/Prueba.java
@@ -1,16 +1,16 @@
 import java.sql.*;
 
-public class App {
+public class Prueba {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/empleadoss_departamentoss?useSSL=false";
-        String sql = "SELECT codDepto, nombreDpto, FROM departamentos";
+        String sql = "SELECT codDepto, nombreDpto FROM departamentos";
 
         try (Connection conecction = DriverManager.getConnection(url, "root", "root");
                 Statement intruccion = conecction.createStatement();
                 ResultSet rS = intruccion.executeQuery(sql);) {
 
             while (rS.next()) {
-                System.out.print("Departamento: " + rS.getInt("codDpto"));
+                System.out.print("Departamento: " + rS.getInt("codDepto"));
                 System.out.print(" Nombre: " + rS.getString("nombreDpto") + "\n");
             }
 
